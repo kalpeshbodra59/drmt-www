60295bfac404759315f550c5b2059d63240ecc8f
Kalpesh Bodra <kalpeshbodra59@gmail.com>
2023-03-05 17:02:40 +0530

    FINAL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://sore-gold-adder-tutu.cyclic.app/api",
+  baseURL: "http://localhost:5000",
   timeout: 10000,
 });
 
